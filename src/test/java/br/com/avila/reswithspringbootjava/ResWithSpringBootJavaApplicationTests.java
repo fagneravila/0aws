26f15f9518bdf8e00f1443ccf3d8355d4d09683d
Fagner Avila <fagner.avila@gmail.com>
2022-09-15 01:14:09 -0300

    inicio

---
+++ src/test/java/br/com/avila/reswithspringbootjava/ResWithSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.avila.reswithspringbootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResWithSpringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
